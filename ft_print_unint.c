b7526baf105420af8b599134d8e1f60b7cc89c0c
Keita Ogitsu <kogitsu@c1r26s6.42tokyo.jp>
2023-02-26 17:03:23 +0900

    modified

--- ft_print_unint.c
+++ ft_print_unint.c
@@ -6,7 +6,7 @@
 /*   By: kogitsu <kogitsu@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 11:34:56 by kogitsu           #+#    #+#             */
-/*   Updated: 2023/02/26 12:36:09 by kogitsu          ###   ########.fr       */
+/*   Updated: 2023/02/26 16:58:42 by kogitsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,7 @@ static void	ft_putunnbr(unsigned int num, int *n)
 		ft_putunnbr(num % 10, n);
 	}
 	else
-	{
-		ft_putchar((char)(num + '0'));
-		(*n)++;
-	}
+		*n += ft_putchar((char)(num + '0'));
 }
 
 void	ft_print_unint(const char **fmt, int *n, va_list *ap)
